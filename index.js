6683c6c7af4de1518ab7467e799a2c2728e42279
Arlefreak <arlefreak@gmail.com>
2015-12-20 23:53:26 -0600

    Updated .gitignore for vim and OSX files Updated to-markdown Updated index.js for new to-markdown version Added new test file

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
+/*jslint node: true */
 'use strict';
+
 var gutil = require('gulp-util'),
     through = require('through2'),
-    toMarkdown = require('to-markdown').toMarkdown;
+    toMarkdown = require('to-markdown');
 
-module.exports = function () {
-  return through.obj(function (file, enc, cb) {
-    var md;
+module.exports = function() {
+    return through.obj(function(file, enc, cb) {
+        var md;
 
-    if (file.isNull()) {
-      cb(null, file);
-      return;
-    }
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
 
-    if (file.isStream()) {
-      cb(new gutil.PluginError('gulp-to-markdown', 'Streaming not supported'));
-      return;
-    }
+        if (file.isStream()) {
+            cb(new gutil.PluginError('gulp-to-markdown', 'Streaming not supported'));
+            return;
+        }
 
-    try {
-      md = toMarkdown(file.contents.toString());
-    } catch (e) {
-      cb(new gutil.PluginError('gulp-to-markdown', e, {fileName: file.path}));
-      return;
-    }
+        try {
+            md = toMarkdown(file.contents.toString());
+        } catch (e) {
+            cb(new gutil.PluginError('gulp-to-markdown', e, {
+                fileName: file.path
+            }));
+            return;
+        }
 
-    file.contents = new Buffer(md);
-    file.path = gutil.replaceExtension(file.path, '.md');
-    cb(null, file);
-  });
+        file.contents = new Buffer(md);
+        file.path = gutil.replaceExtension(file.path, '.md');
+        cb(null, file);
+    });
 };
